4e5255ff452aa0d264477a95d01dc3d731fcc3a2
mayank05942 <mayanknautiyal88@gmail.com>
2024-02-07 21:38:11 +0100

    solved issue with setup

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'torch'
-        'numpy'
+        'torch',  # <-- Add a comma here
+        'numpy',
     ],
-    
 )
